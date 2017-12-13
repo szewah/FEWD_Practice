c51dca48f3f0b3726e0005794b8db45c8f58aab9
szewah <32065713+szewah@users.noreply.github.com>
2017-12-13 16:13:53 -0500

    accordian practice

---
+++ accordian/accordian practice/script.js
@@ -0,0 +1,12 @@
+$('.row').click(showRow)
+
+function showRow() {
+	$(this).find('.wrapper').slideToggle();
+}
+
+
+// $('.row').click(slideRow)
+
+// function slideRow() {
+// 	$(this).find('.wrapper').slideToggle() 
+// }
